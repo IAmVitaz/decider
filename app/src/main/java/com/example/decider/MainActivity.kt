a5fcc6e0dfc2a89ecf5fd1519ec8942fa8125d0c
vit_az <vit.azarov@gmail.com>
2020-06-03 17:48:10 -0300

    initial commit

---
+++ app/src/main/java/com/example/decider/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.decider
+
+import android.app.AlertDialog
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView.OnItemLongClickListener
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    val list = arrayListOf("Watch TV", "Save the world!", "Make a photo of Spider Man")
+    var prevItemDisplayed: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        listView.adapter = adapter
+
+        diceLable.setOnClickListener {
+
+            deciderResult.text = ". . ."
+
+            val handler = Handler()
+            handler.postDelayed(Runnable {
+
+                val random = Random()
+                var randomItem = random.nextInt(list.count())
+
+                // prevent from getting same result twice in a row
+                while (randomItem == prevItemDisplayed) {
+                    randomItem = random.nextInt(list.count())
+                }
+                prevItemDisplayed = randomItem
+
+                deciderResult.text = list[randomItem]
+
+            }, 500)
+
+        }
+
+        addNewItemButton.setOnClickListener {
+            if (addNewItemEditText.text.toString() != "") {
+                val newItem = addNewItemEditText.text.toString()
+                list.add(newItem)
+                addNewItemEditText.text.clear()
+                adapter.notifyDataSetChanged()
+
+                //hide soft keyboard
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+            }
+        }
+
+        viewList.setOnClickListener {
+            if (listViewLayout.visibility == View.INVISIBLE) {
+                listViewLayout.visibility = View.VISIBLE
+                viewList.text = "Hide list"
+            }
+            else {
+                listViewLayout.visibility = View.INVISIBLE
+                viewList.text = "View list"
+            }
+        }
+
+
+        listView.onItemLongClickListener =
+            OnItemLongClickListener { parent, view, position, id ->
+                AlertDialog.Builder(this@MainActivity)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Are you sure?")
+                    .setMessage("Do you want to delete this item?")
+                    .setPositiveButton(
+                        "Yes"
+                    ) { dialog, which ->
+                        list.removeAt(position)
+                        adapter.notifyDataSetChanged()
+                    }
+                    .setNegativeButton("No", null)
+                    .show()
+                true
+            }
+
+    }
+}
